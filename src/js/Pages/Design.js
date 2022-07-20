4ac61b995a570d07fe922f4e9f56a25cb126504e
jacksonlpeterson <jacksonlpeterson@gmail.com>
2022-07-20 13:35:48 -0600

    First Push

---
+++ src/js/Pages/Design.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Design = () => {
+    return(
+
+            <Header />
+        
+    )
+
+}
+
+export default Design;
