85b31e4b95ffaa9c096419cd66a67ba90bf39e06
jacksonlpeterson <jacksonlpeterson@gmail.com>
2022-07-26 15:25:14 -0400

    Forgot to add the card changes

--- src/js/Pages/Design.js
+++ src/js/Pages/Design.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Header from "../components/Header";
+import Project from "../components/Project";
+import Stack from 'react-bootstrap/Stack';
 
 const Design = () => {
     return(
-
+        <div>
             <Header />
+            <h3 style={{marginLeft:"5%"}}>Current project <i class="fa-solid fa-circle-plus"></i></h3>
+            <Stack direction="horizontal" gap={3}>
+                <Project />
+                <Project />
+                <Project />
+             </Stack>
+
+        </div>
         
     )
 
---
+++ src/js/components/Project.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import { Button } from "react-bootstrap";
+import Stack from 'react-bootstrap/Stack';
+
+const Project = () => {
+    return(
+    <Card style={{ width: '20rem', margin:"50px"}}>
+      <Card.Body>
+        <Card.Title>Dumpster Fires</Card.Title>
+        <Card.Text>
+         <Stack gap={3}>
+            <div>150 Assets</div>
+            <div>15 Traits</div>
+            <div>1.5 GB</div>
+         </Stack>
+        </Card.Text>
+        <Button variant="outline-dark" style={{width: '100px', fontSize:'18px', margin:'20px', fontFamily:'poppins', fontWeight:'500'}}>Design</Button>
+        <Button variant="outline-dark" style={{width: '100px', fontSize:'18px', margin:'20px', fontFamily:'poppins', fontWeight:'500'}}>Publish</Button>
+      </Card.Body>
+    </Card>
+    )
+}
+
+export default Project;
