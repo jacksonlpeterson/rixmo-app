47b7a5f90eb5a4024e72cc0b7a5a072a9131eba3
jacksonlpeterson <jacksonlpeterson@gmail.com>
2022-07-26 16:01:33 -0400

    A little broken

--- src/js/Pages/ProjectDesign.js
+++ src/js/Pages/ProjectDesign.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
+import ProjectDetails from "../components/ProjectDetails";
+import ProjectNotes from "../components/ProjectNotes";
+import Card from 'react-bootstrap/Card';
+
 
 const ProjectDesign = () => {
     return(
         <div>
             <Header />
             <h1 style={{marginLeft:'50px'}}>Dumpster Fires</h1>
+            <Card>
+                <ProjectDetails />
+                <ProjectNotes />
+            </Card>
         </div>
     )
 }
---
+++ src/js/components/ProjectDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectDetails = () => {
+    return(
+        <div>Hello</div>
+    )
+}
+
+export default ProjectDetails;
